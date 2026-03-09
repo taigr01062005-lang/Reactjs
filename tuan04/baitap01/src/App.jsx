4e631040fb6c85a58f30b3dab458938152c7d635
H82M19\Student <Student>
2026-03-09 07:45:37 +0700

    bai1

--- tuan04/baitap01/src/App.jsx
+++ tuan04/baitap01/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() =>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data =>{
+      setUsers(data);
+      setLoading(false);
+    }) 
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+        <h3>Danh sách người dùng</h3>
+        {loading ? (
+          <p>Đang tải...</p>
+        ) : (
+          <div className='user-list'>
+            {users.map(user => (
+              <div key={user.id} className='user-item'>
+                <h3>{user.name}</h3>
+                <p>{user.email}</p>
+              </div>
+            ))}
+          </div>
+        )}
+    </div>
   )
 }
 
-export default App
+export default App;

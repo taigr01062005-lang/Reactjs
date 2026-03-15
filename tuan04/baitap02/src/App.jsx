d31c133fafcd01c427552441a4e97a8c2bce5780
taigr01062005-lang <taigr01062005@gmail.com>
2026-03-15 16:27:58 +0700

    bai 2

--- tuan04/baitap02/src/App.jsx
+++ tuan04/baitap02/src/App.jsx
@@ -1,68 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-
-  const [users, setUsers] = useState([]);      
-  const [loading, setLoading] = useState(true); 
-  const [error, setError] = useState(null);  
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchUsers = async () => {
+    const fetchData = async () => {
       try {
-        setLoading(true); 
-        setError(null);   
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');
-        if (!response.ok) {
-          throw new Error('Không thể lấy dữ liệu từ máy chủ!');
-        }
-        const data = await response.json();
-        setUsers(data); 
-
+        setLoading(true);
+        setError(null);
+        const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+        setData(res.data);
       } catch (err) {
-        setError(err.message); 
+        console.error("Lỗi API: ", err);
+        setError("Không thể tải danh sách người dùng. Vui lòng thử lại");
       } finally {
         setLoading(false);
       }
-    };
-
-    fetchUsers();
+    }
+    fetchData();
   }, []);
 
+
   return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h3>Bài 2: Loading & Error State</h3>
-
-      {error && (
-        <div style={{ color: 'red', padding: '20px', border: '1px solid red' }}>
-          <h3> Đã xảy ra lỗi:</h3>
-          <p>{error}</p>
-          <button onClick={() => window.location.reload()}>Thử lại</button>
+    <div className='App'>
+      <h2>Danh Sách User</h2>
+      {/* TH1: Đang Tải Dữ Liệu */}
+      {loading && (
+        <div className='loader'>
+          <h3>Đang tải dữ liệu. Vui lòng chờ!</h3>
         </div>
       )}
 
-      {loading && !error && (
-        <div className="loader">
-          <p style={{ fontSize: '20px', fontWeight: 'bold' }}> Đang tải dữ liệu, vui lòng đợi...</p>
+      {/* TH2: Có Lỗi Xảy Ra */}
+      {!loading && error && (
+        <div style={{ color: 'red', border: '1px solid red', padding: '10px' }}>
+          {error}
         </div>
       )}
 
-      {!loading && !error && (
-        <ul style={{ listStyle: 'none', padding: 0, textAlign: 'left', maxWidth: '400px', margin: '0 auto' }}>
-          {users.map(user => (
-            <li key={user.id} style={{ 
-              padding: '10px', 
-              borderBottom: '1px solid #ccc',
-              marginBottom: '10px'
-            }}>
-              <strong>Name:</strong> {user.name} <br />
-              <strong>Email:</strong> {user.email}
-            </li>
+      {/* TH3: Load Được Dữ Liệu */}
+      {!loading && !error &&(
+        <div className='user-list'>
+          {data.map((user) => (
+            <div key={user.id}>
+              <h3>{user.name}</h3>
+              <p>Email: {user.email}</p>
+            </div>
+                        
           ))}
-        </ul>
+        </div>
       )}
     </div>
-  );
+
+  )
 }
 
 export default App;

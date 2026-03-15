ea6c35fe04dbb5bae4130d0ecc5e0b108b984eeb
taigr01062005-lang <taigr01062005@gmail.com>
2026-03-15 20:52:23 +0700

    bai04

--- tuan04/baitap04/src/App.jsx
+++ tuan04/baitap04/src/App.jsx
@@ -1,38 +1,69 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const App = () => {
-   const [goc, setGoc] =  useState([])
-   const [sauLoc, setSauLoc] = useState([])
-   const [sreach , setSreach] = useState("")
+  const [posts, setPosts] = useState([]);
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  useEffect(()=>{
-    fetch("https://jsonplaceholder.typicode.com/posts")
-    .then(res => res.json())
-    .then(data => {setGoc(data); setSauLoc(data)})
-    
-  }, [])
+  // useEffect(()=>{
+  //   fetch("https://jsonplaceholder.typicode.com/posts")
+  //   .then(res => res.json())
+  //   .then(data => {setGoc(data); setSauLoc(data)})
 
-  const handleSreach = (e) =>{ 
-      const value = e.target.value
-      setSreach(value)
+  // }, [])
 
-      const kq = goc.filter((a) =>
-        a.title.toLowerCase().includes(value.toLowerCase())
-      )
-      setSauLoc(kq)
-    }
-    
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+        setPosts(res.data);
+      } catch (err) {
+        setError("Không tìm thấy!");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPosts();
+  }, []);
+  const filteredPosts = posts.filter((post) =>
+    post.title.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div>
-      <input type="text" value={sreach} onChange={handleSreach} />
-      {sauLoc.map((a) =>(
-        <div key={a.id}>
-          KQ :{a.title}
+      <div className='App'>
+        <h2>Fetch & Search Filter</h2>
+
+        <div style={{marginBottom: '20px'}}>
+            <input type="text"
+                   placeholder='Tìm kiếm theo tiêu đề'
+                   value={search}
+                   onChange={(e) => setSearch(e.target.value)} />
         </div>
-      ))}
-      
-    </div>
+
+        <hr />
+
+        {loading && <p>Đang tải bài viết...</p>}
+
+        {error && <p style={{color: 'red'}}>{error}</p>}
+
+        {!loading && !error && (
+          <div>
+            <p>Hiển thị <strong>{filteredPosts.length}</strong>/{posts.length}</p>
+            {filteredPosts.length >0 ? (
+              filteredPosts.map((post) =>(
+                <div key={post.id}>
+                  <h4>{post.title}</h4>
+                  <p>{post.body}</p>
+                </div>
+              ))
+            ):(
+              <p>Không tìm thấy bài viết khớp với từ khóa!</p>
+            )}
+          </div>
+        )}
+      </div>
   )
 }
-
-export default App
+  export default App

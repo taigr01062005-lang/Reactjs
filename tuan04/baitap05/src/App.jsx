1ec78b82ddb7ff1fc99c01e44e6de211c0e12fe9
taigr01062005-lang <taigr01062005@gmail.com>
2026-03-15 23:22:40 +0700

    bai 05

--- tuan04/baitap05/src/App.jsx
+++ tuan04/baitap05/src/App.jsx
@@ -1,80 +1,94 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+ 
+const url ="https://jsonplaceholder.typicode.com/todos";
 
 const App = () => {
-  const [posts, setPosts] = useState([]);
-  const [search, setSearch] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [todos, setTodos] = useState([]);
+  const [newTitle, setNewTitle] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
   const [error, setError] = useState(null);
 
-  // useEffect(() => {
-  //   axios.get("https://jsonplaceholder.typicode.com/posts")
-  //   .then((res) => {
-  //     setPosts(res.data);
-  //     setLoading(false);
-  //   })
-  //   .catch((err) => {
-  //     setError("Lỗi tải dữ liệu");
-  //     setLoading(false);
-  //   })
-  // },[])
+  useEffect(() => {
+    const fetchToDos = async () => {
+      try {
+        const res = await axios.get(`${url}?_limit=100`);
+        setTodos(res.data);
+      } catch (err) {
+        setError("Không tải được danh sách!");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchToDos();
+  }, []);
 
-fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(res => res.json())
-      .then(data => {
-         setPosts(data);
-         setLoading(false);
-      })
+  const addTodo = async (e) => {
+    e.preventDefault();
+    if (!newTitle.trim()) return;
 
-  // useEffect(() => {
-  //   const fetchPosts = async () => {
-  //     try {
-  //       const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
-  //       setPosts(res.data);
-  //     } catch (err) {
-  //       setError("Không tìm thấy!");
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-  //   fetchPosts();
-  // }, []);
-  const filteredPosts = posts.filter((post) =>
-    post.title.toLowerCase().includes(search.toLowerCase())
-  );
+    setSubmitting(true);
+    try {
+      const res = await axios.post(url, {
+        title: newTitle,
+        completed: false,
+        userId: 1
+      });
+      setTodos([res.data, ...todos]); 
+      setNewTitle("");
+    } catch (err) {
+      alert("Thêm mới thất bại!");
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
+  const deleteTodo = async (id) => {
+    const oldTodos = [...todos];
+    setTodos(todos.filter (t => t.id !== id));
+
+    try{
+      await axios.delete(`${url}/${id}`);
+    } catch (err) {
+      alert("Xóa thất bại!")
+      setTodos(oldTodos);
+    }
+  }
   return (
-      <div className='App'>
-        <h2>Fetch & Search Filter</h2>
+      <div>
+        <h2>CRUD</h2>
 
-        <div style={{marginBottom: '20px'}}>
-            <input type="text"
-                   placeholder='Tìm kiếm theo tiêu đề'
-                   value={search}
-                   onChange={(e) => setSearch(e.target.value)} />
-        </div>
-
-        <hr />
-
-        {loading && <p>Đang tải bài viết...</p>}
+        <form onSubmit={addTodo} style={{marginBottom: '20px'}}>
+          <input type="text"
+                 value={newTitle}
+                 onChange={(e) => setNewTitle(e.target.value)}
+                 disabled = {submitting}
+          />
+          <button type='submit' disabled ={submitting || !newTitle}>
+            {submitting ? "Đang thêm..." : "Thêm"}
+          </button>
+        </form>
+        
+        {loading && <p>Đang tải dữ liệu</p>}
 
         {error && <p style={{color: 'red'}}>{error}</p>}
 
-        {!loading && !error && (
-          <div>
-            <p>Hiển thị <strong>{filteredPosts.length}</strong>/{posts.length}</p>
-            {filteredPosts.length >0 ? (
-              filteredPosts.map((post) =>(
-                <div key={post.id}>
-                  <h4>{post.title}</h4>
-                  <p>{post.body}</p>
-                </div>
-              ))
-            ):(
-              <p>Không tìm thấy bài viết khớp với từ khóa!</p>
-            )}
-          </div>
-        )}
+        <ul>
+          {todos.map(todo=> (
+            <li key={todo.id} style={{listStyle: 'none', 
+            padding: '10px', 
+            borderBottom: '1px solid #eee',
+            display: 'flex',
+            justifyContent: 'space-between'}}>
+              <span>{todo.title}</span>
+              <button onClick={() => deleteTodo(todo.id)}
+              style={{color: 'red', cursor: 'pointer'}} >
+                Xóa
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
   )
 }

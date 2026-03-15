00cc4ec8dfb8c5117ed0f0b63f88727caf915c14
taigr01062005-lang <taigr01062005@gmail.com>
2026-03-15 19:32:11 +0700

    bai03

--- tuan04/baitap03/src/App.jsx
+++ tuan04/baitap03/src/App.jsx
@@ -1,39 +1,63 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [id, setId] = useState(null) 
-  const [datas ,setData] = useState(null)
-  const [error,setErr]   = useState("")
+  const [id, setID] = useState("");
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  useEffect (() =>{
-    
-    if(!id) return 
-    if(id < 1 || id >10){
-      setErr("Not Found")
-      setData(null)
-      return
-    }
- 
-      fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then(res => res.json())
-      .then(data => {setData(data) ; setErr("")})
-      .catch((error) => {setErr("Error")
-          console.log(error)
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+      const iD = parseInt(id);
+      try {
+        if (!id) {
+          setData(null);
+          setError(null);
+          return;
+        }
+        if (iD < 1 || iD > 10 || isNaN(iD)) {
+          throw new Error("Không Tìm Thấy User. Vui Lòng Nhập ID Từ 1 Đến 10");
+        }
+        const res = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+        setData(res.data);
+      } catch (err) {
+        console.error("Lỗi API: ", err);
+        setData(null);
+        setError(err.message || "Không thể tải dữ liệu. Vui lòng thử lại");
+      } finally {
+        setLoading(false);
       }
-      )
-  }, [id])
+    };
+    const timer = setTimeout(() => {
+      fetchData();
+    }, 500);
+    return () => clearTimeout(timer);
+  }, [id]);
+  return (
+    <div className='App'>
+      <h2>Tra Cứu Thông Tin User</h2>
+      <div style={{ marginBottom: '20px' }}>
+        <label>Nhập id (1-10): </label>
+        <input type="number"
+          value={id}
+          onChange={(e) => setID(e.target.value)} />
+      </div>
+      <hr />
 
-
-   return (
-    <div>
-      <p>Danh Sách </p>
-      
-       <input type="number" placeholder='Nhập số 1-10' onChange={(e) => setId(Number(e.target.value))}/>
-       {error && <p>{error}</p> }
-       {datas && <div>  <p>{datas.name}</p> <p>{datas.phone}</p> <p>{datas.website}</p> </div> }
-      
+      {loading && <p>Đang Tìm Kiếm...</p>}
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {data && (
+        <div>
+          <p><strong>Name:</strong> {data.name}</p>
+          <p><strong>Email:</strong> {data.email}</p>
+          <p><strong>Website:</strong> {data.website}</p>
+        </div>
+      )}
     </div>
   )
 }
